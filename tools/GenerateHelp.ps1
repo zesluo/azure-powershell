ca181d03d374bcff30638027983f9b768bb636da
wyunchi-ms <yunwang@microsoft.com>
2020-07-20 22:30:36 +0800

    Add generated module list

--- tools/GenerateHelp.ps1
+++ tools/GenerateHelp.ps1
@@ -79,25 +79,7 @@ if ($ValidateMarkdownHelp)
 }
 
 # We need to define new version of module instead of hardcode here
-$NewModules = @("Az.AppConfiguration",
-                "Az.Blockchain",
-                "Az.ConnectedKubernetes",
-                "Az.CustomProviders",
-                "Az.Databricks",
-                "Az.DesktopVirtualization",
-                "Az.Functions",
-                "Az.HanaOnAzure",
-                "Az.ImageBuilder",
-                "Az.ImportExport",
-                "Az.KubernetesConfiguration",
-                "Az.Kusto",
-                "Az.MariaDb",
-                "Az.MonitoringSolutions",
-                "Az.MySql",
-                "Az.Portal",
-                "Az.PostgreSql",
-                "Az.TimeSeriesInsights"
-                )
+$NewModules = (Get-Content -Raw -Path (Join-Path -Path $PSScriptRoot -ChildPath "AutorestGeneratedModuleList.txt")).Split('\r\n')
 if ($GenerateMamlHelp)
 {
     $FilteredMamlHelpFolders = @()
